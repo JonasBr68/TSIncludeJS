2e78ab91db311829d3632a2edd87d8ad8eb2403b
Jonas Brandel <jonas@cloudcraic.com>
2015-12-03 09:54:00 +0100

    Update Tests.ts

--- sample/Tests.ts
+++ sample/Tests.ts
@@ -1,6 +1,6 @@
 /// <reference path="../src/include.ts" />
 /// <reference path="qunit.d.ts" />
-
+var $? any;
 TSIncludeJS.include("https://code.jquery.com/jquery-2.1.4.js")
     .include("http://code.jquery.com/qunit/qunit-1.20.0.js")
     .done(() => {
