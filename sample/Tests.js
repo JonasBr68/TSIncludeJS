6f527726d5b84d5b8c86f56255b9e02f95d0f655
Jonas Brandel <jonas.brandel@gmail.com>
2015-12-03 11:06:48 +0100

    compiled ts

--- sample/Tests.js
+++ sample/Tests.js
@@ -1,7 +1,7 @@
 /// <reference path="../src/include.ts" />
 /// <reference path="qunit.d.ts" />
 /// <reference path="jquery.d.ts" />
-document.addEventListener("onload", function () {
+document.addEventListener("load", function () {
     TSIncludeJS.include("https://code.jquery.com/jquery-2.1.4.js")
         .include("http://code.jquery.com/qunit/qunit-1.20.0.js")
         .done(function () {
